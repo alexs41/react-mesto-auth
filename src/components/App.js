8bd4ae893c2b252ae6f0995430eca585b8f2ca00
Aleksandr Kaigorodov <alexkaygorodov@gmail.com>
2022-08-31 19:29:14 +0300

    В процессе выполнения пункта 5 ПР10

--- src/components/App.js
+++ src/components/App.js
@@ -2,98 +2,129 @@ import '../index.css';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import ImagePopup from './ImagePopup.js';
+
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Mesto</title>
-      </head>
-      <body>
-          <div className="root">
-              <Header />
-              <Main />
-              <Footer />
-              <div className="popup popup_edit-profile">
-                  <div className="popup__container">
-                      <form className="form form_edit-profile" novalidate>
-                          <h3 className="form__header">Редактировать профиль</h3>
-                          <input id="full-name-input" type="text" name="name" className="form__input form__input_full-name" placeholder="Имя" required minlength="2" maxlength="40" />
-                          <span className="full-name-input-error form__input-error"></span>
+    const [isEditAvatarPopupOpen, setEditAvatarPopupState] = useState(false);
+    const [isEditProfilePopupOpen, setEditProfilePopupState] = useState(false);
+    const [isAddPlacePopupOpen, setAddPlacePopupState] = useState(false);
 
-                          <input id="description-input" type="text" name="about" className="form__input form__input_description" placeholder="Профессия" required minlength="2" maxlength="200" />
-                          <span className="description-input-error form__input-error"></span>
-                          <button className="form__submit-button form__submit-button_edit-profile" type="submit">Сохранить</button>
-                          <button className="popup__close-button" type="button"></button>
-                      </form>
-                  </div>
-              </div>
-              <div className="popup popup_add-element">
-                  <div className="popup__container">
-                      <form className="form form_add-element">
-                          <h3 className="form__header">Новое место</h3>
-                          <input id="element-name-input" type="text" name="name" className="form__input form__input_element-name" placeholder="Название" required minlength="2" maxlength="40" />
-                          <span className="element-name-input-error form__input-error"></span>
+    const handleEditAvatarClick = () => {
+        setEditAvatarPopupState(true);
+        // const popupEditAvatar = document.querySelector('.popup_edit-avatar');
+        // popupEditAvatar.classList.add('popup_opened');
+    };
+    const classNamePopupEditAvatar = `popup popup_edit-avatar ${isEditAvatarPopupOpen ? 'popup_opened' : ''}`;
+    const classNamePopupEditProfile = `popup popup_edit-profile ${isEditProfilePopupOpen ? 'popup_opened' : ''}`;
+    const classNamePopupAddPlace = `popup popup_add-element ${isAddPlacePopupOpen ? 'popup_opened' : ''}`;
 
-                          <input id="picture-link-input" type="url" name="link" className="form__input form__input_picture-link" placeholder="Ссылка на картинку" required />
-                          <span className="picture-link-input-error form__input-error"></span>
 
-                          <button className="form__submit-button form__submit-button_add-element" type="submit">Создать</button>
-                          <button className="popup__close-button" type="button"></button>
-                      </form>
-                  </div>
-              </div>
-              <div className="popup popup_element-image">
-                  <div className="popup__container">
-                      <figure className="figure">
-                          <img className="figure__image" />
-                          <figcaption className="figure__caption">Подпись к фото</figcaption>
-                          <button className="popup__close-button" type="button"></button>
-                      </figure>
-                  </div>
-              </div>
-              <div className="popup popup_confirm">
-                  <div className="popup__container">
-                      <form className="form form_confirm-delete">
-                          <h3 className="form__header">Вы уверены?</h3>
-                          <button className="form__submit-button form__submit-button_confirm" type="submit">Да</button>
-                          <button className="popup__close-button" type="button"></button>
-                      </form>
-                  </div>
-              </div>
+    const handleEditProfileClick = () => {
+        setEditProfilePopupState(true);
+        // const popupEditProfile = document.querySelector('.popup_edit-profile');
+        // popupEditProfile.classList.add('popup_opened');
+    };
 
-              <div className="popup popup_edit-avatar">
-                  <div className="popup__container">
-                      <form className="form form_edit-avatar" novalidate>
-                          <h3 className="form__header">Обновить аватар</h3>
+    const handleAddPlaceClick = () => {
+        setAddPlacePopupState(true);
+        // const popupAddPlace = document.querySelector('.popup_add-element');
+        // popupAddPlace.classList.add('popup_opened');
+    }
+    
 
-                          <input id="avatar-link-input" type="url" name="avatar" className="form__input form__input_avatar-link" placeholder="Ссылка на новый аватар" required />
-                          <span className="avatar-link-input-error form__input-error"></span>
+    return (
+        <div className="App">
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Mesto</title>
+            </head>
+            <body>
+                <div className="root">
+                    <Header />
+                    <Main onEditAvatar={handleEditAvatarClick} onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} />
+                    <Footer />
+                    {/* <ImagePopup /> */}
+                    <div className={classNamePopupEditProfile}>
+                        <div className="popup__container">
+                            <form className="form form_edit-profile" novalidate>
+                                <h3 className="form__header">Редактировать профиль</h3>
+                                <input id="full-name-input" type="text" name="name" className="form__input form__input_full-name" placeholder="Имя" required minlength="2" maxlength="40" />
+                                <span className="full-name-input-error form__input-error"></span>
 
-                          <button className="form__submit-button form__submit-button_edit-avatar" type="submit">Сохранить</button>
-                          <button className="popup__close-button" type="button"></button>
-                      </form>
-                  </div>
-              </div>
+                                <input id="description-input" type="text" name="about" className="form__input form__input_description" placeholder="Профессия" required minlength="2" maxlength="200" />
+                                <span className="description-input-error form__input-error"></span>
+                                <button className="form__submit-button form__submit-button_edit-profile" type="submit">Сохранить</button>
+                                <button className="popup__close-button" type="button"></button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className={classNamePopupAddPlace}>
+                        <div className="popup__container">
+                            <form className="form form_add-element">
+                                <h3 className="form__header">Новое место</h3>
+                                <input id="element-name-input" type="text" name="name" className="form__input form__input_element-name" placeholder="Название" required minlength="2" maxlength="40" />
+                                <span className="element-name-input-error form__input-error"></span>
 
-              <template className="element_template" id="element_template">
-                  <div className="element">
-                      <img src="src/images/dombai.jpg" alt="Домбай" className="element__image" />
-                      <h4 className="element__header"></h4>
-                      <div className="like-container">
-                          <button className="like-container__like-button" type="button"></button> 
-                          <p className="like-container__like-count"></p>
-                      </div>
-                      <button className="element__trash-button" type="button"></button>
-                  </div>
-              </template>
-          </div>
-      </body>
-    </div>
-  );
+                                <input id="picture-link-input" type="url" name="link" className="form__input form__input_picture-link" placeholder="Ссылка на картинку" required />
+                                <span className="picture-link-input-error form__input-error"></span>
+
+                                <button className="form__submit-button form__submit-button_add-element" type="submit">Создать</button>
+                                <button className="popup__close-button" type="button"></button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="popup popup_element-image">
+                        <div className="popup__container">
+                            <figure className="figure">
+                                <img className="figure__image" />
+                                <figcaption className="figure__caption">Подпись к фото</figcaption>
+                                <button className="popup__close-button" type="button"></button>
+                            </figure>
+                        </div>
+                    </div>
+                    <div className="popup popup_confirm">
+                        <div className="popup__container">
+                            <form className="form form_confirm-delete">
+                                <h3 className="form__header">Вы уверены?</h3>
+                                <button className="form__submit-button form__submit-button_confirm" type="submit">Да</button>
+                                <button className="popup__close-button" type="button"></button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className={classNamePopupEditAvatar}>
+                        <div className="popup__container">
+                            <form className="form form_edit-avatar" novalidate>
+                                <h3 className="form__header">Обновить аватар</h3>
+
+                                <input id="avatar-link-input" type="url" name="avatar" className="form__input form__input_avatar-link" placeholder="Ссылка на новый аватар" required />
+                                <span className="avatar-link-input-error form__input-error"></span>
+
+                                <button className="form__submit-button form__submit-button_edit-avatar" type="submit">Сохранить</button>
+                                <button className="popup__close-button" type="button"></button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <template className="element_template" id="element_template">
+                        <div className="element">
+                            <img src="src/images/dombai.jpg" alt="Домбай" className="element__image" />
+                            <h4 className="element__header"></h4>
+                            <div className="like-container">
+                                <button className="like-container__like-button" type="button"></button> 
+                                <p className="like-container__like-count"></p>
+                            </div>
+                            <button className="element__trash-button" type="button"></button>
+                        </div>
+                    </template>
+                </div>
+            </body>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ImagePopup.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+
+export default function ImagePopup()  {
+    const [opened, setOpened] = useState(false);
+
+    const popup = document.querySelector('.popup_element-image');
+    const _figureImage = popup.querySelector('.figure__image');
+    const _figureCaption = popup.querySelector('.figure__caption');
+    
+    function open() {
+        popup.classList.add('popup_opened');
+        // document.addEventListener('keydown', _handleEscClose);
+    }
+    function close() {
+        popup.classList.remove('popup_opened');
+        // document.removeEventListener('keydown', _handleEscClose);
+    }
+
+    // function _handleEscClose = (e) => {
+    //     if (e.key === 'Escape') {
+    //         close();
+    //     }
+    // }
+    return (
+        <div className="popup popup_element-image">
+            <div className="popup__container">
+                <figure className="figure">
+                    <img className="figure__image" />
+                    <figcaption className="figure__caption">Подпись к фото</figcaption>
+                    <button className="popup__close-button" type="button"></button>
+                </figure>
+            </div>
+        </div>
+    );
+}
+
+//   constructor( popupSelector ) {
+//     super(popupSelector);
+//     this._figureImage = this._popup.querySelector('.figure__image');
+//     this._figureCaption = this._popup.querySelector('.figure__caption');
+// }
+// open(name, imageUrl) {
+//     this._figureCaption.textContent = name;
+//     this._figureImage.src = imageUrl;
+//     this._figureImage.alt = name;
+//     super.open();
+// }
+
+//   export default class Popup {
+
+//     constructor( popupSelector ) {
+//         this._popup = document.querySelector(popupSelector);
+//     }
+//     open() {
+//         this._popup.classList.add('popup_opened');
+//         document.addEventListener('keydown', this._handleEscClose);
+//     }
+//     close() {
+//         this._popup.classList.remove('popup_opened');
+//         document.removeEventListener('keydown', this._handleEscClose);
+//     }
+//     _handleEscClose = (e) => {
+//         if (e.key === 'Escape') {
+//             this.close();
+//         }
+//     }
+//     setEventListeners() {
+//         this._popup.addEventListener('mousedown', (evt) => { 
+//             if (evt.target === evt.currentTarget || evt.target.classList.contains('popup__close-button')) { 
+//                 this.close(); 
+//             }; 
+//           }); // обработчик на кнопки закрытия поп-ап и темной области поп-ап
+//     }
+// }
